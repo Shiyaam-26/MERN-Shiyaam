98d4f709c397a9d70c343da0afb57c280071511e
Shiyaam-26 <shiyaam2005@gmail.com>
2025-01-23 11:59:59 +0530

    Modified

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <body>
+      <nav>
+          <ul>
+              <li class="Nav-heading"><a href="">Portfolio</a></li>
+              <li class="Nav-topic"><a href="">Home</a></li>
+              <li class="Nav-topic"><a href="">About</a></li>
+              <li class="Nav-topic"><a href="">Services</a></li>
+              <li class="Nav-topic"><a href="">Location</a></li>
+              <li class="Nav-topic"><a href="">Contact</a></li>
+          </ul>
+      </nav>
+</body>
+    </>
+  )
+}
+
+export default App
